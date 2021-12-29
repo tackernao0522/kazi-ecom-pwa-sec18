1f815df6506d30c87b1e1e29eabf41c0510591b3
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-29 21:03:06 +0900

    337 Create Product Details API Part2

--- server/app/Http/Controllers/Admin/ProductDetailController.php
+++ server/app/Http/Controllers/Admin/ProductDetailController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductDetail;
+use App\Models\ProductList;
 use Illuminate\Http\Request;
 
 class ProductDetailController extends Controller
 {
-    //
+    public function productDetails(Request $request, $id)
+    {
+        $id = $request->id;
+        $productDetails = ProductDetail::where('product_id', $id)->get();
+        $productList = ProductList::where('id', $id)->get();
+
+        $item = [
+            'productDetails' => $productDetails,
+            'productList' => $productList,
+        ];
+
+        return $item;
+    }
 }
